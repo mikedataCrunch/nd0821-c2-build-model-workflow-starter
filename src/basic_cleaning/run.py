6ea1c38c3b29987f33a40913578455129c07a9d3
mikedataCrunch <michaeldorosan11@gmail.com>
2021-12-19 03:10:32 +0800

    Added cleaning for lon-lat contraints of the project, see basic_cleaning go function

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,6 +27,8 @@ def go(args):
     logger.info("Dropping Outliers")
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save
     logger.info("Saving cleaned data")
